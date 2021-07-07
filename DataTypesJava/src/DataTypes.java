26b5441afded2d7e10f0029170220af6d346a387
“Pablo <pablorios1603@gmail.com>
2021-07-07 17:23:40 -0500

    Clase 14 -> tipos de datos

---
+++ DataTypesJava/src/DataTypes.java
@@ -0,0 +1,8 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int n = 1234567890;
+        long nL = 123456789024323454L;
+        double nD = 123.4567890456;
+        float nF = 123.456F;
+    }
+}
