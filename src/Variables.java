55917d460bcf70300840c71a80b8d6313855f5b4
“Pablo <pablorios1603@gmail.com>
2021-07-07 16:21:51 -0500

    Clase 10 -> Variables

---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int speed;
+        speed=10;
+        System.out.println(speed);
+        int salary = 1000;
+        String employeeName = "Pablo Rios";
+        System.out.println(employeeName);
+    }
+}
