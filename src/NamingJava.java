90d1aa601d64abcd757ceb8fd9ca844f34e1713c
“Pablo <pablorios1603@gmail.com>
2021-07-07 17:01:27 -0500

    clase 13 -> naming de variables o constantes

---
+++ src/NamingJava.java
@@ -0,0 +1,20 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int cellphone = 33337777;
+        int cellPhone = 33337777;
+        System.out.println(cellphone);
+        System.out.println(cellPhone);
+
+        String $countryName = "Spain";
+        String _backgorundColor = "Green";
+
+        String currency$ = "MXN";
+        String backgorund_color = "BLUE";
+
+        final double PI = 3.14159;
+        final int POSITION = -5;
+        final int MAX_WIDTH = 9999;
+        final int MIN_WIDTH = 1;
+
+    }
+}
