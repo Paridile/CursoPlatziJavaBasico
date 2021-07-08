049df474ff18791d99aefc6145bed51c4cdb4761
“Pablo <pablorios1603@gmail.com>
2021-07-08 10:59:41 -0500

    Clase 22 ->  if

---
+++ DataTypesJava/src/ifStatement.java
@@ -0,0 +1,10 @@
+public class ifStatement {
+    public static void main(String[] args) {
+        boolean isBluetoohEnabled = true;
+        int fileSended = 3;
+        if(isBluetoohEnabled) {
+            fileSended++;
+            System.out.println("Archivo enviado");
+        }
+    }
+}
